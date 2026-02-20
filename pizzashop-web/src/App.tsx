b5cb363183aef05876532e5fcc728dbd332806e3
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-02-20 17:15:40 -0400

    shadcn ui installation

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,6 +1,11 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
-    <h1>hello!! :3</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   )
 }
 
