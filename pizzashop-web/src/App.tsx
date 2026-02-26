688099cd2320af9fd965f2e367a5035c0ec74601
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-02-26 14:06:59 -0400

    implementing react-helmet-async

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,10 +1,14 @@
 import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './pages/routes'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 export function App() {
   return (
-    <RouterProvider router={router}/>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop"/>
+      <RouterProvider router={router}/>
+    </HelmetProvider>
   )
 }
 
