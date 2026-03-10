c2c7fb7c55abaa1a46a9c0200c692e70b5e1cf6e
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-03-10 12:05:00 -0400

    theme toggle

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -3,13 +3,16 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './pages/routes'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop"/>
-      <Toaster richColors/>
-      <RouterProvider router={router}/>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop"/>
+        <Toaster richColors/>
+        <RouterProvider router={router}/>
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
