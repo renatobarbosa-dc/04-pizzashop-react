6659a8c4b63ae75ed61af4501ad0bffb99604981
renatobarbosa-dc <renatobcarvalho21@gmail.com>
2026-03-12 13:45:13 -0400

    orders screen

--- pizzashop-web/src/pages/routes.tsx
+++ pizzashop-web/src/pages/routes.tsx
@@ -4,6 +4,7 @@ import { SignIn } from './auth/sign-in'
 import { AppLayout } from './_layouts/app'
 import { AuthLayout } from './_layouts/auth'
 import { SignUp } from './auth/sign-up'
+import { Orders } from './app/orders/orders'
 
 export const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Dashboard />
+            },
+            {
+                path: '/orders',
+                element: <Orders />
             }
         ]
     },
